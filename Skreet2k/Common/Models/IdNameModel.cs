5560626f14ceb9745702628fe6f9477439a152d0
Alex Kh <avkharkovskiy@gmail.com>
2018-04-19 20:52:32 +0400

    init

---
+++ Skreet2k/Common/Models/IdNameModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Common.Models
+{
+	public class IdNameModel
+	{
+		public IdNameModel()
+		{
+		}
+
+		public IdNameModel(int id, string name)
+		{
+			Id = id;
+			Name = name;
+		}
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
---
+++ Skreet2k/Common/Models/Result.cs
@@ -0,0 +1,28 @@
+﻿namespace Common.Models
+{
+	public class Result<T> : Result
+	{
+		public Result(T content)
+		{
+			Content = content;
+			IsSuccess = true;
+		}
+
+		public T Content { get; set; }
+	}
+
+	public class Result
+	{
+		public Result()
+		{
+		}
+
+		public Result(string errorMessage)
+		{
+			ErrorMessage = errorMessage;
+		}
+
+		public string ErrorMessage { get; set; }
+		public bool IsSuccess { get; set; }
+	}
+}
