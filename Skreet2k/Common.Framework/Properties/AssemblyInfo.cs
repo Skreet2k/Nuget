5af5f6b7ff9e18c1c7830e0b3bbc477a4919e40e
Skreet2k <skreet2k@gmail.com>
2018-06-12 14:27:42 +0400

    move core project to another folder

--- Skreet2k/Common.Framework/Properties/AssemblyInfo.cs
+++ Skreet2k/Common.Framework/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Common.Framework")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © S2k 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
