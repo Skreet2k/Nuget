002046264f65f7ac4e23a18fcb2dad212070e463
alexander.kharkovskiy <alexander.kharkovskiy@simbirsoft.com>
2021-05-13 02:58:24 +0400

    fix problem with creating new resultList

---
+++ Skreet2k.Common/Models/ResultList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Skreet2k.Common.Models
+{
+    /// <summary>
+    /// Result model with list content
+    /// </summary>
+    /// <typeparam name="T">Content type</typeparam>
+    public class ResultList<T> : Result<IEnumerable<T>>
+    {
+        public ResultList() { }
+
+        public ResultList(IEnumerable<T> content): base(content)
+        {
+        }
+        
+        public int Count { get; set; }
+    }
+}
